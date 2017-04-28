31b78f96a15c42e711d853b07b247e1db483071c
Seratnian <sschleier@posteo.de>
2017-04-28 16:38:12 +0200

    Mail Detail Ansicht

--- CuriousCafeClubs/app/src/main/java/com/ccc_game/curiouscafeclubs/MailOverview.java
+++ CuriousCafeClubs/app/src/main/java/com/ccc_game/curiouscafeclubs/MailOverview.java
@@ -1,9 +1,13 @@
 package com.ccc_game.curiouscafeclubs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.GridView;
+import android.widget.ImageView;
+
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -11,7 +15,7 @@ import java.util.Map;
 
 public class MailOverview extends AppCompatActivity
 {
-
+    MailOverview currentView = this;
     List<String> categories;
     List<String> mailList;
     Map<String, List<String>> mails = new LinkedHashMap<String, List<String>>();
@@ -32,7 +36,7 @@ public class MailOverview extends AppCompatActivity
                 R.drawable.face_08, R.drawable.face_09, R.drawable.face_10, R.drawable.face_11,
                 R.drawable.face_12
         };
-        int[] successfull = {
+        int[] successful = {
                 R.drawable.face_13, R.drawable.face_14, R.drawable.face_15, R.drawable.face_16,
                 R.drawable.face_17, R.drawable.face_18, R.drawable.face_19
         };
@@ -41,6 +45,7 @@ public class MailOverview extends AppCompatActivity
         };
         // TODO
 
+        int[][] images = { unread, read, successful, failed };
 
         GridView gridUnread = (GridView) findViewById(R.id.gridUnread);
         gridUnread.setAdapter(new ImageAdapter(this, unread));
@@ -49,10 +54,56 @@ public class MailOverview extends AppCompatActivity
         gridRead.setAdapter(new ImageAdapter(this, read));
 
         GridView gridSuccess = (GridView) findViewById(R.id.gridSuccess);
-        gridSuccess.setAdapter(new ImageAdapter(this, successfull));
+        gridSuccess.setAdapter(new ImageAdapter(this, successful));
 
         GridView gridFailed = (GridView) findViewById(R.id.gridFailed);
         gridFailed.setAdapter(new ImageAdapter(this, failed));
 
+        for (int[] imgs : images)
+        {
+            for (int img_id : imgs)
+            {
+                ImageView view = (ImageView) findViewById(img_id);
+                view.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View view)
+                    {
+                    Intent intent = new Intent(currentView, MailDetail.class);
+                    startActivity(intent);
+                    }
+                });
+
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
